011d211d11f8a55d4c290483fddffccf46343f26
deepak445566 <deepakpandey1212d@gmail.com>
2025-11-20 13:50:40 +0530

    po

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://steady-gnome-40dc34.netlify.app",
+  origin: "https://courageous-druid-7eb911.netlify.app",
   credentials: true
 }));
 
